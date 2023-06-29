3ec67903368965f254d3e0bf6d52b5c05242dcb0
Lucas Mario <lucasmariocontato@gmail.com>
2023-06-28 23:29:41 -0300

    inicial

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function validar(event) { 
+    event.preventDefault();
+}
+
+const a = document.getElementById("A").Value;
+const a = document.getElementById("B").Value;
+
+if (b > A) {
+    alert("VÁLIDAR");
+} else {
+    alert("inválido");
+}
